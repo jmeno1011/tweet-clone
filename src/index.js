1c6a8e3f87cf43b0b7a902796473806ab829b54a
jmeno1011 <jwhltn8282@gmail.com>
2022-11-17 23:09:43 +0900

    add router and component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./component/App";
 import firebaseInit from "./firebase";
 
 console.log(firebaseInit);
