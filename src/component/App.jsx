c2fa8e64c49c41fa7fef3cd8cee20c955360350d
jmeno1011 <jwhltn8282@gmail.com>
2022-11-17 23:31:28 +0900

    using firebase_getAuth in useState

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -3,9 +3,7 @@ import { useState } from "react";
 import AppRouter from "./AppRouter";
 
 function App() {
-  console.log("auth:", authService);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <>
       <AppRouter isLoggedIn={isLoggedIn} />
