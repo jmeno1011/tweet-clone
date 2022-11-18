dfa622d7c6fbee5fafc1abd0f5855dfd84a003b2
jmeno1011 <jwhltn8282@gmail.com>
2022-11-18 21:18:10 +0900

    call firestore and getDocs

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,28 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 import { dbService } from "firebaseInit";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Home() {
   const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+  const getTweet = async () => {
+    const collectionRef = collection(dbService, "tweets");
+    const dbTweets = await getDocs(collectionRef);
+    dbTweets.forEach((document) => {
+      const tweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setTweets((prev) => [...prev, tweetObject]);
+    });
+  };
+  console.log(tweets);
+  useEffect(() => {
+    getTweet();
+  }, []);
   const onSubmit = async (e) => {
     e.preventDefault();
-    const collectionRef = collection(dbService, "tweet");
+    const collectionRef = collection(dbService, "tweets");
     await addDoc(collectionRef, { tweet, createdAt: Date.now() });
     setTweet("");
   };
@@ -28,6 +44,13 @@ function Home() {
         />
         <input type={"submit"} value="tweet" />
       </form>
+      <div>
+        {tweets.map((tweet) => (
+          <div key={tweet.id}>
+            <h4>{tweet.tweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
