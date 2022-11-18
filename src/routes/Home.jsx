3b392fd67c6954bfe8d519fef7b3df0ed5147f64
jmeno1011 <jwhltn8282@gmail.com>
2022-11-18 19:09:47 +0900

    connect firebase firestore

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,14 @@
+import { addDoc, collection } from "firebase/firestore";
+import { dbService } from "firebaseInit";
 import { useState } from "react";
 
 function Home() {
   const [tweet, setTweet] = useState("");
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
+    const collectionRef = collection(dbService, "tweet");
+    await addDoc(collectionRef, { tweet, createdAt: Date.now() });
+    setTweet("");
   };
   const onChange = (e) => {
     const {
