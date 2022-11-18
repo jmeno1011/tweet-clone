0b2f8244cce7e4f2bd812bf7190a64fd9cd326ae
jmeno1011 <jwhltn8282@gmail.com>
2022-11-18 15:52:30 +0900

    add firebase sign in createUserWithEmailAndPassword

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,8 +1,15 @@
+import { async } from "@firebase/util";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { authService } from "firebaseInit";
 import { useState } from "react";
 
 function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
 
   const onChange = (e) => {
     const {
@@ -17,6 +24,31 @@ function Auth() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    if (newAccount) {
+      // create account
+      createUserWithEmailAndPassword(authService, email, password)
+        .then((userCredentail) => {
+          console.log(userCredentail);
+          // signed in
+          // const user = userCredentail.user;
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+    } else {
+      // log in
+      signInWithEmailAndPassword(authService, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+    }
   };
 
   return (
@@ -38,7 +70,10 @@ function Auth() {
           name="password"
           onChange={onChange}
         />
-        <input type={"submit"} value="Log In" />
+        <input
+          type={"submit"}
+          value={newAccount ? "Create Account" : "Log In"}
+        />
       </form>
       <div>
         <button>Continue with Google</button>
