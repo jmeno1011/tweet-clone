ee0ce2a4eb36e08d70ab5b1231abeb1d72fa8f67
jmeno1011 <jwhltn8282@gmail.com>
2022-11-20 22:53:43 +0900

    remove code in profile.jsx

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,15 +1,6 @@
-import { async } from "@firebase/util";
 import { signOut } from "firebase/auth";
-import {
-  collection,
-  getDocs,
-  orderBy,
-  query,
-  QuerySnapshot,
-  where,
-} from "firebase/firestore";
-import { authService, dbService } from "firebaseInit";
-import { useEffect, useState } from "react";
+import { authService } from "firebaseInit";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Profile({ userObj, refreshUser }) {
@@ -19,31 +10,6 @@ function Profile({ userObj, refreshUser }) {
     signOut(authService);
     navigate("/");
   };
-
-  /** 
-   * 
-   getData(){
-     const q = query(collection(db, "cities"), where("capital", "==", true));
-   
-     const querySnapshot = await getDocs(q);
-     querySnapshot.forEach((doc) => {
-       // doc.data() is never undefined for query doc snapshots
-       console.log(doc.id, " => ", doc.data());
-     });
-   }
-  */
-  // const getMyTweet = async () => {
-  //   const collectionRef = collection(dbService, "tweets");
-  //   const q = query(
-  //     collectionRef,
-  //     where("creatorId", "==", userObj.uid),
-  //     orderBy("createdAt", "desc")
-  //   );
-  //   const querySnapshot = await getDocs(q);
-  //   querySnapshot.forEach((doc) => {
-  //     console.log(doc.data());
-  //   });
-  // };
 
   const onChange = (e) => {
     const {
@@ -62,9 +28,6 @@ function Profile({ userObj, refreshUser }) {
     }
   };
 
-  // useEffect(() => {
-  //   getMyTweet();
-  // }, []);
   return (
     <div>
       <form onSubmit={onSubmit}>
