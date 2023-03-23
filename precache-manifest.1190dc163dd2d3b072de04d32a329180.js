c0b0265062e483bef841a5457ce7e0e717f8a07e
jmeno1011 <jwhltn8282@gmail.com>
2023-03-23 23:39:38 +0900

    Updates

---
+++ precache-manifest.1190dc163dd2d3b072de04d32a329180.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "04063f4deef1be2f39a4",
+    "url": "/tweet-clone/static/css/main.666e8cd4.chunk.css"
+  },
+  {
+    "revision": "04063f4deef1be2f39a4",
+    "url": "/tweet-clone/static/js/main.0e5697d3.chunk.js"
+  },
+  {
+    "revision": "8793488a6cb8b2306e5e",
+    "url": "/tweet-clone/static/js/runtime~main.286b8d3d.js"
+  },
+  {
+    "revision": "ccacd81cabee8f2ec121",
+    "url": "/tweet-clone/static/js/2.db84542d.chunk.js"
+  },
+  {
+    "revision": "3a2fafa42ef035dd15f281b3fce37f0d",
+    "url": "/tweet-clone/index.html"
+  }
+];
